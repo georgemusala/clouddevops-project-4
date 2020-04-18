8cc16b9260d2475ce10a42555c9e651e45b016fd
George Musala <george.musala@uk.kps.com>
2020-04-18 16:09:32 +0300

    TASK 6 - deploying kubernetes and saved output logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=georgemusala/predictapp
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker login
+kubectl run predict --image=$dockerpath --generator=run-pod/v1 --port=80 --labels="app=predict,env=prod"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward predict 8000:80
